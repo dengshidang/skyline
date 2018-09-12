d031d5810fc504bfe8ff17d646b3374cae69601d
dengshidang <dengshidang@WIN-3LS35ADKKPH>
2018-09-12 18:11:12 +0800

    register

---
+++ skyline-registry-center/src/main/java/com/skyline/registryCenter/SkylineRegistryCenterApplication.java
@@ -0,0 +1,17 @@
+package com.skyline.registryCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SkylineRegistryCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylineRegistryCenterApplication.class, args);
+
+	}
+}
